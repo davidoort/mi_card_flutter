1dcd76b26f7ad526332b23464a16ed0d5ee84550
David Oort Alonso <davidoort@hotmail.com>
2020-04-02 16:02:51 +0200

    layout challenge with containers

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,34 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Container(
-            color: Colors.red,
-            child: Text('Hello'),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                width: 100.0,
+                color: Colors.red,
+              ),
+              Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      height: 100,
+                      color: Colors.yellow,
+                    ),
+                    Container(
+                      height: 100,
+                      color: Colors.green,
+                    ),
+                  ],
+                ),
+                width: 100,
+              ),
+              Container(
+                width: 100,
+                color: Colors.blue,
+              ),
+            ],
           ),
         ),
       ),
