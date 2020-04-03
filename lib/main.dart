84b91018a1dd05bf562e23551c3662a605738409
David Oort Alonso <davidoort@hotmail.com>
2020-04-03 10:31:41 +0200

    micard almost done

--- lib/main.dart
+++ lib/main.dart
@@ -11,32 +11,107 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Column(
             children: <Widget>[
-              Container(
-                width: 100.0,
-                color: Colors.red,
+              SizedBox(
+                height: 150,
+              ),
+              CircleAvatar(
+                radius: 70,
+                backgroundImage: AssetImage('images/profile_pic.jpg'),
+              ),
+              Stack(
+                children: <Widget>[
+                  // Stroked text as border.
+                  Text(
+                    'David Oort Alonso',
+                    style: TextStyle(
+                      fontSize: 40,
+                      fontFamily: 'Lobster',
+                      foreground: Paint()
+                        ..style = PaintingStyle.stroke
+                        ..strokeWidth = 6
+                        ..color = Colors.grey[800],
+                    ),
+                  ),
+                  // Solid text as fill.
+                  Text(
+                    'David Oort Alonso',
+                    style: TextStyle(
+                      fontSize: 40,
+                      fontFamily: 'Lobster',
+                      color: Colors.grey[100],
+                    ),
+                  ),
+                ],
+              ),
+              Text(
+                'FLUTTER WANNABE',
+                style: TextStyle(
+                  fontFamily: 'SourceSans',
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.teal.shade100,
+                  letterSpacing: 2,
+                ),
               ),
               Container(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
+                margin:
+                    EdgeInsets.only(top: 20, bottom: 5, left: 30, right: 30),
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10)),
+                child: Row(
+                  // mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
-                    Container(
-                      height: 100,
-                      color: Colors.yellow,
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal.shade100,
                     ),
-                    Container(
-                      height: 100,
-                      color: Colors.green,
+                    SizedBox(
+                      width: 30,
+                    ),
+                    Text(
+                      '+34 123 456 789',
+                      style: TextStyle(
+                        fontFamily: 'SourceSans',
+                        fontSize: 17,
+                      ),
                     ),
                   ],
                 ),
-                width: 100,
               ),
               Container(
-                width: 100,
-                color: Colors.blue,
+                margin: EdgeInsets.only(left: 30, right: 30),
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10)),
+                child: Row(
+                  // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Icon(
+                      Icons.mail_outline,
+                      color: Colors.teal.shade100,
+                    ),
+                    SizedBox(
+                      width: 30,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          'davidoort@email.com',
+                          style: TextStyle(
+                            fontFamily: 'SourceSans',
+                            fontSize: 17,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
